ac4548071de5c80bb207f5ed63458820cead17d9
wll <iwanglulu@gmail.com>
2017-06-06 01:14:58 +0800

    add value_of appendList_term

--- src/runtime.ml
+++ src/runtime.ml
@@ -60,6 +60,43 @@ let rec removeLinList =
     | _ -> raise (RuntimeError ("removeList only accept LinList!"))
 
 
+let rec print_addr =
+    fun hp addr ->
+    match hp with
+    | EmptyHeap -> raise (RuntimeError ("print_addr can not find the addr in heap!"))
+    | ExtendHeap (a,v,h) -> if a = addr
+                            then (match v with
+                                  | LinResVal str -> print_string ("(" ^ str ^ ") ")
+                                  | LinListVal (addr1,addr2) -> if addr1 = -1 && addr2 = -1
+                                                                then print_string "null "
+                                                                else begin
+                                                                        print_addr !heap addr1;
+                                                                        print_addr !heap addr2
+                                                                     end
+                                  | _ -> raise (RuntimeError ("error! heap can not store non resource value!")))
+                            else print_addr h addr
+
+let rec changeAddr =
+    fun hp addrx addr2 ->
+    match hp with
+    | EmptyHeap -> raise (RuntimeError ("changeAddr error!"))
+    | ExtendHeap (a,v,h) -> if a = addr2
+                            then ExtendHeap (addrx,v,h) 
+                            else ExtendHeap (a,v,(changeAddr h addrx addr2))
+
+let rec appendAddr =
+    fun addrx addr2 -> 
+    let hv = lookup_heap !heap addrx in
+    match hv with
+    | LinListVal (x1,x2) -> if x1 = -1 && x2 = -1
+                            then begin
+                                     heap := delete_heap !heap addrx;
+                                     heap := changeAddr !heap addrx addr2
+                                 end
+                            else appendAddr x2 addr2
+    | _ -> raise (RuntimeError ("appendAddr error1"))
+
+
 let rec value_of_tm =
     fun tm stk ->
     match tm with
@@ -128,7 +165,7 @@ let rec value_of_tm =
                                   | ClosureVal (str,tm,stk) -> let v = value_of_tm tm2 stk in
                                                                value_of_tm tm (ExtendStack (str,v,stk))
                                   | _ -> raise (RuntimeError ("need a function!")))
-    | Begin_term     tmlist -> 
+    | Begin_term     tmlist -> value_of_tmlist tmlist stk
     | LetUn_term     (str,tm1,tm2) -> let v = value_of_tm tm1 stk in
                                       value_of_tm tm2 (ExtendStack (str,v,stk))
     | LetLin_term    (str,tm1,tm2) -> let v = value_of_tm tm1 stk in
@@ -152,7 +189,25 @@ let rec value_of_tm =
                                                         | _ -> raise (RuntimeError ("copyAtom only accept LinRes!")))
                                                 | _ -> raise (RuntimeError ("copyAtom only accept resources!")))
     | CopyList_term     (tm1,str1,str2,tm2) -> 
-    | AppendList_term   (tm1,tm2) -> 
+    | AppendList_term   (tm1,tm2) -> let v1 = value_of_tm tm1 stk in
+                                     let v2 = value_of_tm tm2 stk in
+                                     (match v1,v2 with
+                                     | AddrVal addr1, AddrVal addr2 -> 
+                                            let hv1 = lookup_heap !heap addr1 in
+                                            let hv2 = lookup_heap !heap addr2 in
+                                            (match hv1,hv2 with
+                                            | LinListVal (a1,a2), LinListVal (b1,b2) -> 
+                                                if a1 = -1 && a2 = -1
+                                                then begin 
+                                                        heap := delete_heap !heap addr1;
+                                                        AddrVal addr2
+                                                     end
+                                                else begin
+                                                        appendAddr a2 addr2;
+                                                        AddrVal addr1
+                                                     end
+                                            | _ -> raise (RuntimeError ("appendList only accepty LinList!")))
+                                     | _ -> raise (RuntimeError ("appendList only accept resources!")))
     | Split_term        (tm1,str1,str2,tm2) -> let v = value_of_tm tm1 stk in
                                                (match v with
                                                | AddrVal addr -> 
@@ -188,7 +243,10 @@ let rec value_of_tm =
                                                     UnitVal
                                                 end
                               | _ -> raise (RuntimeError ("freeList only accept resources!")))
-    | Print_term        tm -> 
+    | Print_term        tm -> let v = value_of_tm tm stk in
+                              (match v with
+                              | AddrVal addr -> print_addr !heap addr; v
+                              | _ -> raise (RuntimeError ("print only accept resources!")))
     | LinCons_term      (tm1,tm2) -> let v1 = value_of_tm tm1 stk in
                                      let v2 = value_of_tm tm2 stk in
                                      (match v1,v2 with
@@ -199,6 +257,16 @@ let rec value_of_tm =
     | Null_term         ->  addressCount := !addressCount + 1;
                             heap := ExtendHeap (!addressCount,(LinListVal (-1,-1)),!heap);
                             AddrVal !addressCount
+
+and value_of_tmlist =
+    fun tmlist stk ->
+    match tmlist with
+    | [] -> raise (RuntimeError ("begin can not empty term"))
+    | tm::[] -> value_of_tm tm stk
+    | tm::li -> let _ = value_of_tm tm stk in
+                value_of_tmlist li stk
+
+
 
 let value_of_program =
     fun ast ->
